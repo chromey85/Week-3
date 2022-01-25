1569c74c3b472db96a4f79418c331ee63bc0f8aa
chromey85 <yayyub85@gmail.com>
2022-01-25 14:55:32 +0000

    1st Upload

---
+++ 1 - Add Event Listener/app.js
@@ -0,0 +1,7 @@
+console.log("I promise you I'm Linked")
+
+const myHeading = document.getElementById("heading");
+
+myHeading.addEventListener("mouseover", () => {
+    myHeading.style.color = "red";
+});
