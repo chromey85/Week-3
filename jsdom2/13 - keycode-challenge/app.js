6b642f4d6a4dd5f27d6580f7ceadcedcdab0f683
chromey85 <yayyub85@gmail.com>
2022-01-27 18:30:01 +0000

    more tasks and edits

--- jsdom2/13 - keycode-challenge/app.js
+++ jsdom2/13 - keycode-challenge/app.js
@@ -1,12 +1,12 @@
 const output = document.querySelectorAll(".output-box");
 const big = document.querySelector(".output");
+const cover = document.querySelector("h2");
+
 
 document.addEventListener("keyup", (i) => {
     console.log(i);
     output[0].innerHTML = i.key;
     output[1].textContent = i.keyCode;
     output[2].textContent = i.code;
-    big.innerHTML = i.keyCode
+    big.innerHTML = i.keyCode;
 })
-
-// .output.content = i.keyCode
