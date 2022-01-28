60b7e53a3ca084e98cc9be9d53e04b6522591fb1
chromey85 <yayyub85@gmail.com>
2022-01-28 00:24:45 +0000

    edits

--- jsdom2/13 - keycode-challenge/app.js
+++ jsdom2/13 - keycode-challenge/app.js
@@ -1,10 +1,14 @@
 const output = document.querySelectorAll(".output-box");
 const big = document.querySelector(".output");
-const cover = document.querySelector("h2");
-
+const cover = document.querySelector(".text");
+const cards = document.querySelectorAll(".output, .event");
 
 document.addEventListener("keyup", (i) => {
     console.log(i);
+    cover.style.display = "none"
+    cover.style.index = "-1"
+    cards[0].style.display = "flex"
+    cards[1].style.display = "flex"
     output[0].innerHTML = i.key;
     output[1].textContent = i.keyCode;
     output[2].textContent = i.code;
